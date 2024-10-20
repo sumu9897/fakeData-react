de012033d794f2d9b0002d80c69e35274b6ea37f
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-21 01:33:28 +0600

    Product add in Cart site

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Product from './components/Product';
 
 function App() {
   const [products ,setProducts] =useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(()=>{
     fetch("../public/fakeData.json")
@@ -12,19 +13,50 @@ function App() {
     .then(data =>{
       setProducts(data)
     })
-  },[])
-  console.log(products)
+  },[]);
+
+  const handleCart = (p) =>{
+    const isExists = cart.find((product) => product.id ==p.id)
+    if(!isExists){
+      setCart([...cart,p])
+    }else{
+      alert("Cart already exists");
+    }
+
+  };
+  console.log(cart)
+
+  // console.log(products)
   return (
     <>
 
       <div className="main-container">
         <div className="cards-container">
           {
-            products.map(product =><Product product={product}></Product>)
+            products.map(product =><Product
+              product={product} 
+              handleCart={handleCart}></Product>)
           }
           
         </div>
-        <div className="card-container">Card</div>
+        <div className="card-container">
+        <h3>Cart Items</h3>
+          <div className="cart-title">
+            <h5>Name :</h5>
+            <h5>Price :</h5>
+          </div>
+          <div >
+          {
+            cart.map((item) => (
+              <div className="cart-info">
+                <h5>{item.title.slice(0,10)}</h5>
+                <h5>{item.price}</h5>
+              </div>
+            ))
+          }
+
+          </div>
+        </div>
       </div>
       
     </>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Product = ({product}) => {
-    console.log(product)
+const Product = ({product, handleCart}) => {
+    // console.log(handleCart)
     return (
         <div>
             <div className="card">
             <img className='card-img' src={product.image} alt="" />
-            <h3>{product.title}</h3>
-            <p><b>Discription : </b>{product.description}</p>
+            <h3>{product.title.slice(0,10)}</h3>
+            <p><b>Discription : </b>{product.description.slice(0,50)}</p>
             <div className="card-footer">
-              <h1>Price:{product.price}</h1>
-              <button className='add-btn'>Add To Cart</button>
+              <h4>Price:{product.price}$</h4>
+              <button onClick={() => handleCart(product)} className='add-btn'>Add To Cart</button>
             </div>
           </div>
         </div>
